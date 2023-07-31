894e9a2938ff8b66d02e40532962fc0b60e1fd3d
Juan <4lzeck@gmail.com>
2023-07-31 14:17:14 +0100

    feat(plugins): Add lualine

--- lua/alzeck/lazy.lua
+++ lua/alzeck/lazy.lua
@@ -117,13 +117,38 @@ local plugins = {
   },
 
   'NvChad/nvim-colorizer.lua',
-
   {
     'numToStr/Comment.nvim',
     config = function()
       require('Comment').setup()
     end
   },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    event = 'ColorScheme',
+    config = function()
+      require('lualine').setup({
+        options = {
+          -- @usage 'rose-pine' | 'rose-pine-alt'
+          theme = 'rose-pine-alt',
+        },
+        sections = {
+          lualine_c = {
+            'filetype',
+            {
+              'filename',
+              path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+            }
+          },
+
+          lualine_x = {},
+          lualine_y = {},
+        }
+      })
+    end
+  }
 }
 
 local opts = {
