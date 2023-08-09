6305b28a56a13154f43ef43e8035196533fe0069
Juan <4lzeck@gmail.com>
2023-08-09 23:24:57 +0100

    fix(lsp): CVA support in tailwind

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,11 +3,11 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'tsserver',
-  'rust_analyzer',
-  'tailwindcss',
-  'jsonls',
-  'elixirls'
+	"tsserver",
+	"rust_analyzer",
+	"tailwindcss",
+	"jsonls",
+	"elixirls",
 })
 
 -- Fix Undefined global 'vim'
@@ -16,85 +16,118 @@ lsp.nvim_workspace()
 local null_ls = require("null-ls")
 
 null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.autopep8,
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.mix,
-    null_ls.builtins.diagnostics.credo,
-    null_ls.builtins.diagnostics.eslint,
-  },
+	sources = {
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.autopep8,
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.mix,
+		null_ls.builtins.diagnostics.credo,
+	},
 })
 
 lsp.format_on_save({
-  format_opts = {
-    async = false,
-    timeout_ms = 10000,
-  },
-  servers = {
-    ['rust_analyzer'] = { 'rust' },
-    -- if you have a working setup with null-ls
-    -- you can specify filetypes it can format.
-    ['null-ls'] = {
-      'markdown', 'graphql', 'html', 'css', 'scss', 'less', 'yaml',
-      'javascript', 'javascriptreact', 'typescript', 'typescriptreact',
-      'markdown.mdx', 'python', 'elixir', 'json', 'jsonc', 'vue', 'lua',
-      'svelte'
-    },
-  }
+	format_opts = {
+		async = false,
+		timeout_ms = 10000,
+	},
+	servers = {
+		["rust_analyzer"] = { "rust" },
+		-- if you have a working setup with null-ls
+		-- you can specify filetypes it can format.
+		["null-ls"] = {
+			"markdown",
+			"graphql",
+			"html",
+			"css",
+			"scss",
+			"less",
+			"yaml",
+			"javascript",
+			"javascriptreact",
+			"typescript",
+			"typescriptreact",
+			"markdown.mdx",
+			"python",
+			"elixir",
+			"json",
+			"jsonc",
+			"vue",
+			"lua",
+			"svelte",
+		},
+	},
 })
 
-local cmp = require('cmp')
+local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+	["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
 })
 
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
+cmp_mappings["<Tab>"] = nil
+cmp_mappings["<S-Tab>"] = nil
 
 lsp.setup_nvim_cmp({
-  formatting = {
-    format = require("tailwindcss-colorizer-cmp").formatter
-  },
-  mapping = cmp_mappings
+	formatting = {
+		format = require("tailwindcss-colorizer-cmp").formatter,
+	},
+	mapping = cmp_mappings,
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = { buffer = bufnr, remap = false }
+	local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("n", "gd", function()
+		vim.lsp.buf.definition()
+	end, opts)
+	vim.keymap.set("n", "K", function()
+		vim.lsp.buf.hover()
+	end, opts)
+	vim.keymap.set("n", "<leader>vws", function()
+		vim.lsp.buf.workspace_symbol()
+	end, opts)
+	vim.keymap.set("n", "<leader>vd", function()
+		vim.diagnostic.open_float()
+	end, opts)
+	vim.keymap.set("n", "[d", function()
+		vim.diagnostic.goto_next()
+	end, opts)
+	vim.keymap.set("n", "]d", function()
+		vim.diagnostic.goto_prev()
+	end, opts)
+	vim.keymap.set("n", "<leader>vca", function()
+		vim.lsp.buf.code_action()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrr", function()
+		vim.lsp.buf.references()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrn", function()
+		vim.lsp.buf.rename()
+	end, opts)
+	vim.keymap.set("i", "<C-h>", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
 end)
 
-local lspconfig = require("lspconfig")
-
-lspconfig.tailwindcss.setup({
-  settings = {
-    tailwindCSS = {
-      experimental = {
-        classRegex = {
-          { "cva\\(([^)]*)\\)",
-           "[\"'`]([^\"'`]*).*?[\"'`]" },
-        },
-      },
-    },
-  },
+lsp.configure("tailwindcss", {
+	settings = {
+		tailwindCSS = {
+			classAttributes = { "classNames" },
+			experimental = {
+				classRegex = {
+					{ "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+					{ "cn\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+				},
+			},
+		},
+	},
 })
 
 lsp.setup()
 
 vim.diagnostic.config({
-  virtual_text = true
+	virtual_text = true,
 })
