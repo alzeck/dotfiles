fcc2305934d098fb986a8e29c3320f3456b197d7
Juan <4lzeck@gmail.com>
2024-01-23 23:03:15 +0000

    feat: harpoon2

--- lua/alzeck/plugins/navigation.lua
+++ lua/alzeck/plugins/navigation.lua
@@ -33,24 +33,35 @@ return {
 	},
 	{
 		"theprimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
-			local mark = require("harpoon.mark")
-			local ui = require("harpoon.ui")
+			local harpoon = require("harpoon")
+			harpoon:setup({})
 
-			vim.keymap.set("n", "<leader>a", mark.add_file)
-			vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+			vim.keymap.set("n", "<leader>a", function()
+				harpoon:list():append()
+			end)
+			vim.keymap.set("n", "<C-e>", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list(), {
+					border = "rounded",
+					title_pos = "center",
+					title = " Harpoon ",
+					ui_max_width = 80,
+				})
+			end)
 
 			vim.keymap.set("n", "<C-a>", function()
-				ui.nav_file(1)
+				harpoon:list():select(1)
 			end)
 			vim.keymap.set("n", "<C-s>", function()
-				ui.nav_file(2)
+				harpoon:list():select(2)
 			end)
 			vim.keymap.set("n", "<C-d>", function()
-				ui.nav_file(3)
+				harpoon:list():select(3)
 			end)
 			vim.keymap.set("n", "<C-f>", function()
-				ui.nav_file(4)
+				harpoon:list():select(4)
 			end)
 		end,
 	},
