1f49a1682e3efd0762b7e9ffcdb9fe60b3bedce6
Juan <4lzeck@gmail.com>
2024-02-08 10:32:24 +0000

    feat: add folding

--- lua/alzeck/plugins/base.lua
+++ lua/alzeck/plugins/base.lua
@@ -29,6 +29,56 @@ return {
 		dependencies = {},
 	},
 
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = {
+			{ "kevinhwang91/promise-async" },
+		},
+		config = function()
+			vim.o.foldcolumn = "0"
+			vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
+
+			local handler = function(virtText, lnum, endLnum, width, truncate)
+				local newVirtText = {}
+				local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+				local sufWidth = vim.fn.strdisplaywidth(suffix)
+				local targetWidth = width - sufWidth
+				local curWidth = 0
+				for _, chunk in ipairs(virtText) do
+					local chunkText = chunk[1]
+					local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					if targetWidth > curWidth + chunkWidth then
+						table.insert(newVirtText, chunk)
+					else
+						chunkText = truncate(chunkText, targetWidth - curWidth)
+						local hlGroup = chunk[2]
+						table.insert(newVirtText, { chunkText, hlGroup })
+						chunkWidth = vim.fn.strdisplaywidth(chunkText)
+						-- str width returned from truncate() may less than 2nd argument, need padding
+						if curWidth + chunkWidth < targetWidth then
+							suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+						end
+						break
+					end
+					curWidth = curWidth + chunkWidth
+				end
+				table.insert(newVirtText, { suffix, "MoreMsg" })
+				return newVirtText
+			end
+
+			require("ufo").setup({
+				enable_get_fold_virt_text = true,
+				fold_virt_text_handler = handler,
+			})
+
+			-- Using ufo provider need remap `zR` and `zM`.
+			vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+			vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+		end,
+	},
+
 	-- LSP Support
 	{
 		"neovim/nvim-lspconfig",
@@ -40,9 +90,6 @@ return {
 			{ "williamboman/mason-lspconfig.nvim" },
 		},
 		config = function()
-			require("neodev").setup({
-				-- add any options here, or leave empty to use the default settings
-			})
 			local lsp_zero = require("lsp-zero")
 			lsp_zero.extend_lspconfig()
 
@@ -58,6 +105,17 @@ return {
 				-- to learn the available actions
 				lsp_zero.default_keymaps({ buffer = bufnr })
 			end)
+
+			lsp_zero.set_server_config({
+				capabilities = {
+					textDocument = {
+						foldingRange = {
+							dynamicRegistration = false,
+							lineFoldingOnly = true,
+						},
+					},
+				},
+			})
 
 			lsp_zero.format_on_save({
 				format_opts = {
@@ -247,8 +305,7 @@ return {
 			})
 		end,
 	},
-	-- Copilot
-	--"github/copilot.vim",
+
 	{
 		"numToStr/Comment.nvim",
 		config = true,
